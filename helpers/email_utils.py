48566cc281cadfe7e44cbf9ed7365d9bc39886eb
anavasynth <vadimgonchariuk@gmail.com>
2025-04-27 02:28:09 +0300

    update webhook smtp

--- helpers/email_utils.py
+++ helpers/email_utils.py
@@ -2,13 +2,9 @@ from flask_mail import Message
 from __init__ import mail  # імпортуй `mail`, який ініціалізував у app.py
 from flask import current_app
 
-def send_email(subject, recipients, body=None, html_body=None):
+def send_email(subject, recipients, body):
     try:
-        msg = Message(subject=subject, recipients=recipients)
-        if html_body:
-            msg.html = html_body
-        if body:
-            msg.body = body
+        msg = Message(subject, recipients=recipients, body=body)
         mail.send(msg)
     except Exception as e:
-        current_app.logger.error(f"Помилка при відправці листа: {e}")
+        current_app.logger.error(f"Помилка відправки листа: {e}")
